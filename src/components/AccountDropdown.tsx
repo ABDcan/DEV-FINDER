f5080b4e68fa1b26aa03bb19e9845453186ebe48
ahoang2502 <athoang252@gmail.com>
2024-05-10 11:29:42 +1000

    feat: delete account

--- src/components/AccountDropdown.tsx
+++ src/components/AccountDropdown.tsx
@@ -1,59 +1,93 @@
 "use client";
 
-import React, { useState } from "react";
-import { signOut, useSession } from "next-auth/react";
 import { DeleteIcon, LogOutIcon } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
+import { deleteAccountAction } from "@/app/actions";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export const AccountDropdown = () => {
   const session = useSession();
   const [open, setOpen] = useState(false);
-
+  
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="link" className="px-0">
-          <Avatar className="mr-2">
-            <AvatarImage
-              src={session.data?.user?.image ?? "/default-user.jpg"}
-            />
-            <AvatarFallback className="rounded-full border border-gray-300">
-              CN
-            </AvatarFallback>
-          </Avatar>
+    <>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently remove your
+              account and any data associated with it.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
 
-          {session.data?.user?.name}
-        </Button>
-      </DropdownMenuTrigger>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                await deleteAccountAction();
 
-      <DropdownMenuContent>
-        <DropdownMenuItem
-          onClick={() =>
-            signOut({
-              callbackUrl: "/",
-            })
-          }
-        >
-          <LogOutIcon className="mr-3 w-5 h-5" />
-          Sign out
-        </DropdownMenuItem>
+                signOut({ callbackUrl: "/" });
+              }}
+            >
+              Yes, delete my account
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
 
-        <DropdownMenuItem
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          <DeleteIcon className="mr-3 h-5 w-5" /> Delete Account
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="link" className="px-0">
+            <Avatar className="mr-2">
+              <AvatarImage
+                src={session.data?.user?.image ?? "/default-user.jpg"}
+              />
+              <AvatarFallback className="rounded-full border border-gray-300">
+                CN
+              </AvatarFallback>
+            </Avatar>
+
+            {session.data?.user?.name}
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() =>
+              signOut({
+                callbackUrl: "/",
+              })
+            }
+          >
+            <LogOutIcon className="mr-3 w-5 h-5" />
+            Sign out
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <DeleteIcon className="mr-3 h-5 w-5" /> Delete Account
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
